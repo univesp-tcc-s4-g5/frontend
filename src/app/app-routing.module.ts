7715677d162ff3446a40559bc573991d21a77bfd
Hugueds <hugo-pdf@hotmail.com>
2022-05-22 23:47:22 -0300

    add more routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BalanceComponent } from './pages/balance/balance.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { QuestionComponent } from './pages/question/question.component';
 import { RankingComponent } from './pages/ranking/ranking.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { SearchComponent } from './pages/search/search.component';
@@ -11,8 +13,10 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'ranking', component: RankingComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'search', component: SearchComponent },
+  { path: 'registrar', component: RegisterComponent },
+  { path: 'base-de-conhecimento', component: SearchComponent },
+  { path: 'perguntas', component: QuestionComponent },
+  { path: 'saldo', component: BalanceComponent },
   { path: '**', redirectTo: 'login' }
 ];
 
