6f0c4b54aacb94e6715582f6638d6bd3460bc23c
Hugo Peres de Freitas <hugo.freitas@scania.com>
2022-05-29 16:35:09 -0300

    updates from work computer

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { LoggedUser } from 'src/app/models/LoggedUser';
 import { LoginService } from 'src/app/services/login/login.service';
 
@@ -10,11 +11,19 @@ import { LoginService } from 'src/app/services/login/login.service';
 export class HeaderComponent implements OnInit {
 
   public loggedUser?: LoggedUser;
+  public show = true;
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService
+    , private router: Router) { }
 
   ngOnInit(): void {
     this.loggedUser = this.loginService.getLoggedUser();
+    if (!this.loggedUser) {
+      console.log('No user logged in');
+      // this.show = false;
+      return;
+    }
+    this.show = true;
   }
 
 }
